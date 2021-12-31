9ab0b8019e6c2279cb5a4ca2e03da2d380854c75
Kunlei Lian <kunlei1987@gmail.com>
2021-12-30 20:30:01 -0600

    updat tabu search

--- src/main/java/com/or/heuristic/core/algo/tabusearch/basic/TsApplicable.java
+++ src/main/java/com/or/heuristic/core/algo/tabusearch/basic/TsApplicable.java
@@ -5,18 +5,22 @@ import com.or.heuristic.core.util.Optimizable;
 import java.util.List;
 
 /**
+ * This interface defines functions that should be implemented by solutions in order to enable tabu search.
+ *
  * @author Kunlei Lian
  */
 public interface TsApplicable<K> extends Optimizable {
   /**
+   * This function create a list of neighboring solutions from the current solution.
    *
-   * @return
+   * @return neighboring solutions
    */
   List<? extends TsApplicable<K>> getNeighbors(int count);
 
   /**
+   * This function retrieves the tabu key of the current solution.
    *
-   * @return
+   * @return tabu table key
    */
   K getTabuKey();
 }
--- src/main/java/com/or/heuristic/core/algo/tabusearch/basic/TsConfig.java
+++ src/main/java/com/or/heuristic/core/algo/tabusearch/basic/TsConfig.java
@@ -4,15 +4,31 @@ import lombok.Builder;
 import lombok.Getter;
 
 /**
+ * This class defines the required parameters of the tabu search algorithm.
  *
  * @author Kunlei Lian
  */
 @Getter
 @Builder
 public final class TsConfig {
+  /**
+   * the number of iterations that a move is forbidden
+   */
   private final int tabuLength;
+  /**
+   * the number of neighboring solutions to explore at each iteration
+   */
   private final int neighborSize;
+  /**
+   * the maximum number of iterations to search
+   */
   private final int maxIter;
+  /**
+   * the maximum number of iterations that the best solution couldn't be improved
+   */
   private final int maxIterNoImprove;
+  /**
+   * maximum runtime in seconds
+   */
   private final int maxRuntimeInSecs;
 }
