6814fbd3f6bcf6619b609d32608b4082a7cea89e
Kunlei Lian <kunlei1987@gmail.com>
2022-01-18 21:18:50 -0600

    update genetic algorithm

--- src/main/java/com/or/heuristic/core/algo/geneticalgo/GeneticAlgorithm.java
+++ src/main/java/com/or/heuristic/core/algo/geneticalgo/GeneticAlgorithm.java
@@ -4,6 +4,7 @@ import com.or.heuristic.core.util.*;
 
 import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -17,6 +18,7 @@ public class GeneticAlgorithm extends Algorithm {
   private final List<? extends GaApplicable> startingPopulation;
   private GaApplicable bestSolution;
   private final SolutionComparator<GaApplicable> solutionComparator;
+  private final SecureRandom random;
 
   public GeneticAlgorithm(ObjectiveSense objectiveSense, GaConfig gaConfig,
                           List<? extends GaApplicable> startingPopulation) {
@@ -30,15 +32,18 @@ public class GeneticAlgorithm extends Algorithm {
       .limit(1)
       .collect(Collectors.toList())
       .get(0);
+    this.random = new SecureRandom();
   }
 
   @Override
   public void solve() {
+    int populationSize = gaConfig.getPopulationSize();
     int maxIter = gaConfig.getMaxIter();
     int maxIterNoImprove = gaConfig.getMaxIterNoImprove();
     int maxRuntimeInSecs = gaConfig.getMaxRuntimeInSecs();
     double mutationProbability = gaConfig.getMutationProbability();
     double elitePreserveProportion = gaConfig.getElitePreserveProportion();
+    int numEliteSolutionsToPreserve = (int) (populationSize * elitePreserveProportion);
 
     SecureRandom random = new SecureRandom();
     long startTime = System.currentTimeMillis();
@@ -57,8 +62,20 @@ public class GeneticAlgorithm extends Algorithm {
       });
 
       // combine the two population
-      currPopulation.addAll(newPopulation);
-      currPopulation.sort(solutionComparator);
+      newPopulation.addAll(currPopulation);
+      currPopulation.clear();
+      newPopulation.stream()
+          .sorted(this.solutionComparator)
+          .limit(numEliteSolutionsToPreserve)
+          .forEach(currPopulation::add);
+
+      List<GaApplicable> nonEliteSolutions = newPopulation.stream()
+        .sorted(this.solutionComparator)
+        .skip(numEliteSolutionsToPreserve)
+        .collect(Collectors.toList());
+      while (currPopulation.size() < populationSize) {
+        currPopulation.add(nonEliteSolutions.get(random.nextInt(nonEliteSolutions.size())));
+      }
 
       // check stopping criteria
       long elapsedSecs = TimeUnit.SECONDS
@@ -82,15 +99,44 @@ public class GeneticAlgorithm extends Algorithm {
     List<Double> objectives = currPopulation.stream()
       .map(Optimizable::getObjective)
       .collect(Collectors.toList());
+    DoubleSummaryStatistics objSummary = objectives.stream()
+      .mapToDouble(Double::doubleValue)
+      .summaryStatistics();
+    List<Double> normalizedObjectives = new ArrayList<>(populationSize);
+    if (objectiveSense == ObjectiveSense.MAXIMIZE) {
+      objectives.stream()
+        .map(o -> o / objSummary.getMax())
+        .forEach(normalizedObjectives::add);
+    } else {
+      objectives.stream()
+        .map(o -> objSummary.getMax() - o)
+        .map(o -> o / objSummary.getMax() + 1.0)
+        .forEach(normalizedObjectives::add);
+    }
+    double sumNormalizedObj = normalizedObjectives.stream()
+      .mapToDouble(Double::doubleValue)
+      .summaryStatistics()
+      .getSum();
 
     List<GaApplicable> newPopulation = new ArrayList<>(populationSize);
-    for (int i = 0; i < populationSize; i++) {
-
+    while (newPopulation.size() < populationSize) {
+      int parent1Idx = rouletteWheelSelection(normalizedObjectives, sumNormalizedObj, random.nextDouble());
+      int parent2Idx = rouletteWheelSelection(normalizedObjectives, sumNormalizedObj, random.nextDouble());
+      GaApplicable child = currPopulation.get(parent1Idx).crossover(currPopulation.get(parent2Idx));
+      child.computeObjective();
+      newPopulation.add(child);
     }
     return newPopulation;
   }
 
-  private int rouletteWheelSelection(List<Double> objectives, double probability) {
-
+  private int rouletteWheelSelection(List<Double> objectives, double totalObj, double probability) {
+    double total = 0;
+    for (int i = 0; i < objectives.size(); i++) {
+      total += objectives.get(i);
+      if (total / totalObj >= probability) {
+        return i;
+      }
+    }
+    return objectives.size() - 1;
   }
 }
